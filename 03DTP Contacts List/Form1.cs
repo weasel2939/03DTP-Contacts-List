b46c65381496728976de31963c1995bc4ef60bd2
Milz Lee <milz.lee@gbhs.school.nz>
2022-05-17 13:11:43 +1200

    V4.31

--- 03DTP Contacts List/Form1.cs
+++ 03DTP Contacts List/Form1.cs
@@ -25,7 +25,7 @@ namespace _03DTP_Contacts_List
 
         public static int selectedIndex = 0;
 
-        string filePath = @"C:\\contactStorage.txt";
+        string filePath = @"contactStorage.txt";
 
 
         // For counting purposes, might have to remove.
